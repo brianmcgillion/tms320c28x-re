2b2e4ebd518c028be9dd9e8082a2142aad00f8cb
Brian McGillion <bmg.avoin@gmail.com>
2026-09-14 22:39:37 +0400

    feat(cleanup): delete old parallel py implementation
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- c28x/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: MIT
-"""TMS320C28x ISA decoder library."""
-
-from c28x.types import DecodedInstruction, Operand, OperandType
-from c28x.decoder import Decoder
-
-__all__ = ["Decoder", "DecodedInstruction", "Operand", "OperandType"]
